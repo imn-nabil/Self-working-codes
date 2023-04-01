e11064f0eb079b67ed0ee1e3dd2dd2eff83dac8b
imn-nabil <imnnabil1971@gmail.com>
2023-04-01 21:42:33 +0600

    second commit before push

---
+++ 100 imn UVA.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int func(int a,int b);
+
+int main()
+{
+    int i,j,result;
+    while(scanf("%d %d",&i,&j)!=EOF){
+        result = func(i , j);
+        printf("%d %d %d\n",i,j,result);
+    }
+
+    return 0;
+}
+
+int func(int a,int b){
+    int x,count = 1,max =0;
+//reversing the values
+    if(a > b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    while(a <= b){
+        x = a;
+        while(x != 1){
+            if(x % 2 == 0) x = x / 2;
+            else x = (3 * x) + 1;
+            count++;
+            if(x == 1)break;
+        }
+        if(count > max) max = count;
+        count = 1;
+        a++;
+    }
+    return max;
+}
